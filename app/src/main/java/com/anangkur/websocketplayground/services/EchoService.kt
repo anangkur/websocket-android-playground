773e6c4ea0dbb8d2c4f847cacccf1eb2660e3ab3
anangkur <anangk97@gmail.com>
2021-10-15 15:49:16 +0700

    feat(app): add function to send and receive data in EchoService.kt

--- app/src/main/java/com/anangkur/websocketplayground/services/EchoService.kt
+++ app/src/main/java/com/anangkur/websocketplayground/services/EchoService.kt
@@ -1,28 +1,15 @@
 package com.anangkur.websocketplayground.services
 
-import android.graphics.Bitmap
-import com.tinder.scarlet.Event
-import com.tinder.scarlet.State
+import com.tinder.scarlet.WebSocket
 import com.tinder.scarlet.ws.Receive
 import com.tinder.scarlet.ws.Send
 import io.reactivex.Flowable
 
 interface EchoService {
-    @Receive
-    fun observeState(): Flowable<State>
 
     @Receive
-    fun observeEvent(): Flowable<Event>
-
-    @Receive
-    fun observeText(): Flowable<String>
-
-    @Receive
-    fun observeBitmap(): Flowable<Bitmap>
+    fun observeConnection(): Flowable<WebSocket.Event>
 
     @Send
-    fun sendText(message: String): Boolean
-
-    @Send
-    fun sendBitmap(message: Bitmap): Boolean
+    fun sendMessage(param: String)
 }
